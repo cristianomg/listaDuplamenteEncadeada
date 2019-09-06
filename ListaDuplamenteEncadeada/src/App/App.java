6236457bcfa5d17fb4ddd41a48bd7be2960b92c0
cristiano Macedo <cristiano.macedo@souunit.com.br>
2019-09-05 23:14:35 -0300

    implementação do app

--- ListaDuplamenteEncadeada/src/App/App.java
+++ ListaDuplamenteEncadeada/src/App/App.java
@@ -5,6 +5,7 @@
  */
 package App;
 
+import Crud.Classes.Bem;
 import Crud.Classes.Pessoa;
 import ListaDuplamenteEncadeada.classes.ListaDuplamenteEncadeada;
 import java.util.Scanner;
@@ -18,11 +19,12 @@ public class App {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int app = 1;
         ListaDuplamenteEncadeada<Pessoa> pessoas = new ListaDuplamenteEncadeada();
         String nome;
+        String nomeBem;
         while (app == 1){
             System.out.println("1- Incluir Pessoa");
             System.out.println("2- Excluir Pessoa");
@@ -37,34 +39,115 @@ public class App {
                     nome = sc.next();
                     Pessoa p = new Pessoa(nome);
                     pessoas.adicionar(p);
-                        break;
+                    System.out.println("Pessoa cadastrada com sucesso!!!");
+                    System.out.println("Codigo de acesso: " + p.getCodigo()); 
+                       break;
                 case 2:
                     System.out.println("Informe o nome: ");
                     nome = sc.next();
                     int pos = 0;
                     boolean removido = false;
-                    for(Pessoa pe: pessoas){
-                        if (pe.getNome().equals(nome)){
+                    try{
+                        for(Pessoa pe: pessoas){
+                            if (pe.getNome().equals(nome)){
                             pessoas.remover(pos);
-                            System.out.println("Pessoa Removida!!");
+                            System.out.println("Pessoa Removida com sucesso!!");
                             removido = true;
+                            }
+                            pos++;
                         }
-                        pos++;
                         if (!removido){
                             System.out.println("Pessoa não encontrada!!!");
-                        }
+                    }
+                    }
+                    catch(NullPointerException e){
+                        System.out.println("Nenhum cadastro no sistema!!!");
                     }
                     break;
                 case 3:
-                    break;
-                case 4:
-                    for(Pessoa pessoa: pessoas){
-                        System.out.println(pessoa.getNome() + pessoa.getCodigo());
+                    try{
+                        System.out.println("Informe o nome: ");
+                        nome = sc.next();
+                        boolean encontrado = false;
+                        for(Pessoa pe: pessoas){
+                            if(pe.getNome().equals(nome)){
+                                System.out.printf("%-7s%-10s%s%s\n", "Cod Pessoa: ", pe.getCodigo(),"Nome: ", pe.getNome());
+                                System.out.println("Relação de bens");
+                                System.out.printf("%-8s%-10s%-23s%s\n", "CodBem", "Nome do bem", "", "Valor");
+                                System.out.printf("%-8s%-5s%-16s%s\n", "------", "------------", "--------------", "-----------");
+                                for (Bem b: pe.getListaBens()){
+                                    System.out.printf("%-8d%-10s%-19s%-,5.2f%n\n", b.getCodigo(), b.getNome(), "", b.getValor());
+                                }
+                                encontrado = true;
+                            }
+                            if(!encontrado){
+                                System.out.println("Pessoa não encontrada no sistema!!!");
+                            }
+                        }
+                    }
+                    catch(NullPointerException e){
+                        
                     }
                     break;
+                case 4:
+                    if (pessoas.tamanho()>= 1){
+                        System.out.printf("%-10s%2s%-25s%2s%-20s\n", "Cod Pessoa", " ", "Nome da Pessoa", " ", "Valor Total Bens");
+                        System.out.printf("%-10s%2s%-25s%2s%-20s\n", "----------", " ", "------------------------", " ", "----------------");
+                        for(Pessoa pessoa: pessoas){
+                            System.out.printf("%10s%2s%-25s%2s%-,5.2f%n\n", pessoa.getCodigo(), " ", pessoa.getNome(), " ", pessoa.valorTotalBens());
+
+                    }
+                    }
+                    else{
+                        System.out.println("Nenhum cadastro no sistema!!!");
+                    }
+
+                    break;
                 case 5:
+                    System.out.print("Informe o Codigo da pessoa: ");
+                    int codigo = sc.nextInt();
+                    for(Pessoa pessoa: pessoas){
+                        if(pessoa.getCodigo()==codigo){
+                            System.out.print("Informe o codigo do bem: ");
+                            int codigoBem = sc.nextInt();
+                            System.out.print("Informe o nome do bem: ");
+                            nomeBem = sc.next();
+                            System.out.print("Informe o valor do bem: ");
+                            double valorBem = sc.nextDouble();
+                            try{
+                                Bem bem = new Bem(codigoBem, nomeBem, valorBem);
+                                pessoa.adicionarBem(bem);
+                                System.out.println("Bem cadastrado com sucesso!!!");
+                            }
+                            catch (Exception e){
+                                System.out.println("Erro: " + e.getMessage());
+                            }
+                        }
+                    }
                     break;
                 case 6:
+                    System.out.print("Informe o Codigo da pessoa: ");
+                    codigo = sc.nextInt();
+                    for(Pessoa pessoa: pessoas){
+                        if(pessoa.getCodigo()==codigo){
+                            System.out.print("Informe o codigo do bem: ");
+                            int codigoBem = sc.nextInt();
+                            try{
+                                pos = 0;
+                                for(var b: pessoa.getListaBens()){
+                                    if(b.getCodigo()== codigoBem){
+                                        pessoa.getListaBens().remover(pos);
+                                        System.out.println("Bem removido com sucesso!!!");
+                                        break;
+                                    }
+                                    pos++;
+                                }
+                            }
+                            catch (NullPointerException e){
+                                System.out.println("Erro: Nenhum bem cadastrado!!!");
+                            }
+                        }
+                    }
                     break;
                 default:
                     System.out.println("Opção incorreta!!!");
